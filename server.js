b37f8ba830d295cd8fef0f8d0a7a3cc4d32287c0
mblevu <brianislevu@gmail.com>
2024-03-17 13:35:02 +0300

    Add UsersController and User routes

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const routes = require('./routes/index');
+const router = require('./routes/index');
+
+const port = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use('/', routes);
+app.use(express.json());
+app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`server running on port ${port}`);
 });
+
+export default app;
