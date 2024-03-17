4be07ad442d15ebf4daf8fd7813b703a260d4c68
mblevu <brianislevu@gmail.com>
2024-03-17 13:04:38 +0300

    Add AppController, routes, and server.js files

---
+++ controllers/AppController.js
@@ -0,0 +1,16 @@
+const redisClient = require('../utils/redis');
+const dbClient = require('../utils/db');
+
+class AppController {
+  static getStatus(request, response) {
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  }
+
+  static async getStats(request, response) {
+    const usersNum = await dbClient.nbUsers();
+    const filesNum = await dbClient.nbFiles();
+    response.status(200).json({ users: usersNum, files: filesNum });
+  }
+}
+
+module.exports = AppController;
