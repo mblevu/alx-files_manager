d0838193bf6603c2688d5a1064504e41423321fe
mblevu <brianislevu@gmail.com>
2024-03-17 22:54:44 +0300

    Add FilesController and postUpload route

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const UserController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.post('/users', UserController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UserController.getMe);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
